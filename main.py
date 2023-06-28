28671dae016ab26158ed6305bc350e00214d66d4
ukxxx <42536544+ukxxx@users.noreply.github.com>
2023-06-28 14:08:29 +0300

    Done some code cleaning

--- main.py
+++ main.py
@@ -20,29 +20,24 @@ def main(csv_file):
                 surname = result.split()[2]
             except IndexError:
                 surname = ''
-            # print(lastname, firstname, surname)
 
             # Читаем место работы
             organization = row[3]
-            # print(organization)
 
             # Читаем должность
             position = row[4]
-            # print(position)
 
             # Работаем с номером телефона
             pattern = r'\+?(7?|8)?\s?\s?\(?(\d{3})\)? ?-?(\d{3})-?(\d{2})-?(\d{2}) ?\(? ?(\w?\w?\w?\.)? ?(\d{4})?\)?'
             replace = r'+7(\2)\3-\4-\5 \6\7'
             result = re.sub(pattern, replace, row[5])
             phone = "".join(result.split(','))
-            # print(phone)
 
             # Читаем почту
             email = row[6]
-            # print(email)
 
             full_name = ' '.join([part for part in [lastname, firstname] if part])
-            # print(full_name)   
+  
             if full_name not in contacts:
                 contacts[full_name] = {
                     'lastname': lastname,
@@ -54,12 +49,6 @@ def main(csv_file):
                     'email': email
                 }
             else:
-                # Объединение информации в случае дублирующейся записи
-                # existing_contact = contacts[full_name]
-                # existing_contact['organization'] = existing_contact['organization'] #or row.get('organization', '')
-                # existing_contact['position'] = existing_contact['position'] #or row.get('position', '')
-                # existing_contact['phone'] = existing_contact['phone'] #or row.get('phone', '')
-                # existing_contact['email'] = existing_contact['email'] #or row.get('email', '')
                 if surname:
                     contacts[full_name]['surname'] = surname
                 if organization:
